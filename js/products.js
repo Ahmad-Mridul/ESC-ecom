93ed232658ea3d5330c8b97275093586d378ac0b
Ahmad-Mridul <mridulahmed262@gmail.com>
2026-02-17 00:01:13 +0600

    modal added in products showing title, descriptions, price, rating, buy now and add to cart button

--- js/products.js
+++ js/products.js
@@ -1,5 +1,6 @@
 let ourProducts = document.getElementById("our_products");
 let spinner = document.getElementById("spinner");
+let modalBox = document.getElementById("modal_box");
 // let allProducts = [];
 async function loadProduct() {
     spinner.classList.remove("hidden");
@@ -7,10 +8,10 @@ async function loadProduct() {
         .then(res => res.json())
         .then(data => {
             allProducts = data;
-            setTimeout(() => {
-                displayProducts(data);
-                spinner.classList.add("hidden");
-            }, 3000);
+            displayProducts(data);
+            spinner.classList.add("hidden");
+            // setTimeout(() => {
+            // }, 3000);
         })
 }
 loadProduct()
@@ -49,10 +50,29 @@ const displayProducts = (allProducts) => {
 
 
                 <div class="flex gap-2">
-                    <button class="btn btn-outline-white shadow flex-grow px-5 py-1"><i class="fa-regular fa-eye"></i> Details</button>
+                    <button class="btn btn-outline-white shadow flex-grow px-5 py-1" onclick="my_modal_1.showModal()"><i class="fa-regular fa-eye"></i> Details</button>
                     <button class="btn btn-primary shadow flex-grow"><i class="fa-solid fa-cart-shopping"></i> Add</button>
                 </div>
             </div>
+        `
+        modalBox.innerHTML = `
+        <h3 class="text-lg font-bold">${product.title}</h3>
+            <p class="py-4 text-justify">${product.description}</p>
+            <div class="flex justify-between">
+                <p class="py-2">Price: <span class="font-bold">$${product.price}</span></p>
+                <p class="py-2">Rating: <i class="fa-solid fa-star text-yellow-400"></i> ${product.rating.rate}</p>
+            </div>
+            <div class="mt-5">
+                <button class="btn btn-info text-white">Buy Now</button>
+                <button class="btn btn-warning text-white">Add to Cart</button>
+            </div>
+            <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn">Close</button>
+                </form>
+            </div>
+        
         `
         ourProducts.append(cardDiv);
     });
@@ -96,14 +116,14 @@ const loadCategoryProduct = async (category) => {
     spinner.classList.remove("hidden");
     ourProducts.innerHTML = "";
     const randomDelay = Math.floor(Math.random() * 2000) + 1000;
-    console.log(randomDelay);
+    // console.log(randomDelay);
     await fetch(`https://fakestoreapi.com/products/category/${category}`)
     .then(res => res.json())
     .then(data => {
-        setTimeout(()=>{
-            spinner.classList.add("hidden");
-            displayProducts(data);
-            },randomDelay);
+        spinner.classList.add("hidden");
+        displayProducts(data);
+        // setTimeout(()=>{
+        //     },randomDelay);
         })
 }
 
