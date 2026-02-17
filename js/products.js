5003b1fe47ba43f6e8d9f006c2013a2246bdd5d3
Ahmad-Mridul <mridulahmed262@gmail.com>
2026-02-17 16:49:09 +0600

    add to cart for responsive view for both home page and product page have been implemented

--- js/products.js
+++ js/products.js
@@ -22,14 +22,6 @@ const displayProducts = (allProducts) => {
     ourProducts.innerHTML = "";
     allProducts.map(product => {
         let cardDiv = document.createElement("div");
-        // let productName = document.createElement("p");
-        // let productImage = document.createElement("img");
-        // productImage.src = product.image;
-        // productName.innerText = product.title;
-        // cardDiv.append(productImage);
-        // cardDiv.append(productName);
-        // console.log(cardDiv);
-        // trendingProduct.append(cardDiv);
         const shortTitle = product.title.length > 30 ? product.title.substring(0, 30) + "..." : product.title;
         cardDiv.classList.add("flex", "flex-col", "bg-white", "rounded", "shadow", "w-75");
         cardDiv.innerHTML = `
@@ -57,25 +49,6 @@ const displayProducts = (allProducts) => {
                 </div>
             </div>
         `
-        // modalBox.innerHTML = `
-        // <h3 class="text-lg font-bold">${product.title}</h3>
-        //     <p class="py-4 text-justify">${product.description}</p>
-        //     <div class="flex justify-between">
-        //         <p class="py-2">Price: <span class="font-bold">$${product.price}</span></p>
-        //         <p class="py-2">Rating: <i class="fa-solid fa-star text-yellow-400"></i> ${product.rating.rate}</p>
-        //     </div>
-        //     <div class="mt-5">
-        //         <button class="btn btn-info text-white">Buy Now</button>
-        //         <button class="btn btn-warning text-white" onClick="handleAddToCart(${product.id})">Add to Cart</button>
-        //     </div>
-        //     <div class="modal-action">
-        //         <form method="dialog">
-        //             <!-- if there is a button in form, it will close the modal -->
-        //             <button class="btn">Close</button>
-        //         </form>
-        //     </div>
-
-        // `
         ourProducts.append(cardDiv);
     });
 }
@@ -157,7 +130,7 @@ const openDetailsModal = (productId) => {
             <p class="py-2">Price: <span class="font-bold">$${product.price}</span></p>
             <p class="py-2">Rating: <i class="fa-solid fa-star text-yellow-400"></i> ${product.rating.rate}</p>
         </div>
-        <div class="mt-5">
+        <div class=showModal"mt-5">
             <button class="btn btn-info text-white">Buy Now</button>
             <button class="btn btn-warning text-white" onClick="handleAddToCart(${product.id}); my_modal_1.close()">Add to Cart</button>
         </div>
@@ -172,6 +145,7 @@ const openDetailsModal = (productId) => {
 
 
 let cartNumber = document.getElementById("cartNumber");
+let mobileCartNumber = document.getElementById("mobile-cartNumber");
 let cartItems = parseInt(localStorage.getItem("CartItems")) || 0;
 
 let storedIds = localStorage.getItem("CartProductsIDs");
@@ -182,15 +156,17 @@ const handleAddToCart = (id) => {
     cartItems += 1;
     localStorage.setItem("CartItems", cartItems);
     cartNumber.innerText = cartItems;
+    mobileCartNumber.innerText = cartItems;
 
     cartProductsIDList.push(id);
     localStorage.setItem("CartProductsIDs", cartProductsIDList.join(","));
     loadCartProductsList();
 }
 cartNumber.innerText = cartItems;
+mobileCartNumber.innerText = cartItems;
 
 
-let drawerUl = document.getElementById("drawer-items");
+// let drawerUl = document.getElementById("drawer-items");
 
 
 
@@ -198,11 +174,14 @@ let drawerUl = document.getElementById("drawer-items");
 
 
 let cartTbody = document.getElementById("cart-tbody");
+let mobileCartTbody = document.getElementById("mobile-cart-tbody");
 let cartTotalPriceEl = document.getElementById("cart-total-price");
+let mobileCartTotalPriceEl = document.getElementById("mobile-cart-total-price");
 
 
 const loadCartProductsList = async () => {
     cartTbody.innerHTML = "";
+    mobileCartTbody.innerHTML = "";
 
     let count = 0;
     let currentTotal = 0;
@@ -224,16 +203,25 @@ const loadCartProductsList = async () => {
             const shortTitle = product.title.length > 15 ? product.title.substring(0, 15) + "..." : product.title;
 
             let tr = document.createElement("tr");
+            let mobileTr = document.createElement("tr");
             tr.innerHTML = `
                 <th>${count}</th>
                 <td class="text-sm"">${shortTitle}</td>
                 <td class="font-semibold">$${product.price.toFixed(2)}</td>
                 <td><button class="btn btn-error btn-xs text-white" onClick="handleDeleteCartItem(${product.id})"><i class="fa-solid fa-trash"></i></button></td>
             `;
+            mobileTr.innerHTML = `
+                <th>${count}</th>
+                <td class="text-sm"">${shortTitle}</td>
+                <td class="font-semibold">$${product.price.toFixed(2)}</td>
+                <td><button class="btn btn-error btn-xs text-white" onClick="handleDeleteCartItem(${product.id})"><i class="fa-solid fa-trash"></i></button></td>
+            `;
             cartTbody.append(tr);
+            mobileCartTbody.append(mobileTr);
         }
     });
     cartTotalPriceEl.innerText = `$${currentTotal.toFixed(2)}`;
+    mobileCartTotalPriceEl.innerText = `$${currentTotal.toFixed(2)}`;
 }
 
 
--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+// import { handleAddToCart } from "./products";
+
 let trendingProduct = document.getElementById("trending_product");
 let spinner = document.getElementById("spinner");
 let modalBox = document.getElementById("modal_box");
-let allProducts = [];
+let allFetchedProducts = [];
 
 
 
@@ -10,12 +12,14 @@ async function loadProduct() {
     await fetch("https://fakestoreapi.com/products")
         .then(res => res.json())
         .then(data => {
-            allProducts = data;
-            console.log("all products: ", allProducts);
-            setTimeout(()=>{
-                displayProducts(data);
-                spinner.classList.add("hidden");
-            },3000,);
+            allFetchedProducts = data;
+            // console.log("all products: ", allProducts);
+            displayProducts(data);
+            spinner.classList.add("hidden");
+            // setTimeout(()=>{
+            //     displayProducts(data);
+            //     spinner.classList.add("hidden");
+            // },3000,);
         })
 }
 loadProduct()
@@ -53,32 +57,136 @@ const displayProducts = (allProducts) => {
 
 
                 <div class="flex gap-2">
-                    <button class="btn btn-outline-white shadow flex-grow px-5 py-1" onclick="my_modal_1.showModal()"><i class="fa-regular fa-eye"></i> Details</button>
-                    <button class="btn btn-primary shadow flex-grow"><i class="fa-solid fa-cart-shopping"></i> Add</button>
+                    <button class="btn btn-outline-white shadow flex-grow px-5 py-1" onclick="openDetailsModal(${product.id})"><i class="fa-regular fa-eye"></i> Details</button>
+                    <button class="btn btn-primary shadow flex-grow" onClick="handleAddToCart(${product.id})"><i class="fa-solid fa-cart-shopping"></i> Add</button>
                 </div>
             </div>
             
         `
-        modalBox.innerHTML = `
-        <h3 class="text-lg font-bold">${product.title}</h3>
-            <p class="py-4 text-justify">${product.description}</p>
-            <div class="flex justify-between">
-                <p class="py-2">Price: <span class="font-bold">$${product.price}</span></p>
-                <p class="py-2">Rating: <i class="fa-solid fa-star text-yellow-400"></i> ${product.rating.rate}</p>
-            </div>
-            <div class="mt-5">
-                <button class="btn btn-info text-white">Buy Now</button>
-                <button class="btn btn-warning text-white">Add to Cart</button>
-            </div>
-            <div class="modal-action">
-                <form method="dialog">
-                    <!-- if there is a button in form, it will close the modal -->
-                    <button class="btn">Close</button>
-                </form>
-            </div>
+        // modalBox.innerHTML = `
+        // <h3 class="text-lg font-bold">${product.title}</h3>
+        //     <p class="py-4 text-justify">${product.description}</p>
+        //     <div class="flex justify-between">
+        //         <p class="py-2">Price: <span class="font-bold">$${product.price}</span></p>
+        //         <p class="py-2">Rating: <i class="fa-solid fa-star text-yellow-400"></i> ${product.rating.rate}</p>
+        //     </div>
+        //     <div class="mt-5">
+        //         <button class="btn btn-info text-white">Buy Now</button>
+        //         <button class="btn btn-warning text-white" onClick="handleAddToCart(${product.id})">Add to Cart</button>
+        //     </div>
+        //     <div class="modal-action">
+        //         <form method="dialog">
+        //             <!-- if there is a button in form, it will close the modal -->
+        //             <button class="btn">Close</button>
+        //         </form>
+        //     </div>
         
-        `
+        // `
         trendingProduct.append(cardDiv);
     });
 }
 
+// handleAddToCart();
+
+const openDetailsModal = (productId) => {
+    console.log(productId);
+    const product = allFetchedProducts.find(p => p.id === productId);
+    if (!product) return;
+
+    modalBox.innerHTML = `
+        <h3 class="text-lg font-bold">${product.title}</h3>
+        <p class="py-4 text-justify">${product.description}</p>
+        <div class="flex justify-between">
+            <p class="py-2">Price: <span class="font-bold">$${product.price}</span></p>
+            <p class="py-2">Rating: <i class="fa-solid fa-star text-yellow-400"></i> ${product.rating.rate}</p>
+        </div>
+        <div class="mt-5">
+            <button class="btn btn-info text-white">Buy Now</button>
+            <button class="btn btn-warning text-white" onClick="handleAddToCart(${product.id}); my_modal_1.close()">Add to Cart</button>
+        </div>
+        <div class="modal-action">
+            <form method="dialog">
+                <button class="btn">Close</button>
+            </form>
+        </div>
+    `;
+    my_modal_1.showModal();
+}
+
+
+let cartNumber = document.getElementById("cartNumber");
+let mobileCartNumber = document.getElementById("mobile-cartNumber");
+let cartItems = parseInt(localStorage.getItem("CartItems")) || 0;
+
+let storedIds = localStorage.getItem("CartProductsIDs");
+let cartProductsIDList = storedIds ? storedIds.split(",") : [];
+// localStorage.setItem("CartItems",cartItems);
+const handleAddToCart = (id) => {
+
+    cartItems += 1;
+    localStorage.setItem("CartItems", cartItems);
+    cartNumber.innerText = cartItems;
+    mobileCartNumber.innerText = cartItems;
+
+    cartProductsIDList.push(id);
+    localStorage.setItem("CartProductsIDs", cartProductsIDList.join(","));
+    loadCartProductsList();
+}
+cartNumber.innerText = cartItems;
+mobileCartNumber.innerText = cartItems;
+
+
+
+let cartTbody = document.getElementById("cart-tbody");
+let mobileCartTbody = document.getElementById("mobile-cart-tbody");
+let cartTotalPriceEl = document.getElementById("cart-total-price");
+let mobileCartTotalPriceEl = document.getElementById("mobile-cart-total-price");
+
+
+const loadCartProductsList = async () => {
+    cartTbody.innerHTML = "";
+    mobileCartTbody.innerHTML = "";
+
+    let count = 0;
+    let currentTotal = 0;
+
+    const cartProductsID = localStorage.getItem("CartProductsIDs");
+    const idsArray = cartProductsID ? cartProductsID.split(",").map(Number) : [];
+
+    const res = await fetch("https://fakestoreapi.com/products");
+    const products = await res.json();
+
+    idsArray.forEach(id => {
+        const product = products.find(p => p.id === id);
+
+        if (product) {
+            count += 1;
+            currentTotal += product.price;
+
+
+            const shortTitle = product.title.length > 15 ? product.title.substring(0, 15) + "..." : product.title;
+
+            let tr = document.createElement("tr");
+            let mobileTr = document.createElement("tr");
+            tr.innerHTML = `
+                <th>${count}</th>
+                <td class="text-sm"">${shortTitle}</td>
+                <td class="font-semibold">$${product.price.toFixed(2)}</td>
+                <td><button class="btn btn-error btn-xs text-white" onClick="handleDeleteCartItem(${product.id})"><i class="fa-solid fa-trash"></i></button></td>
+            `;
+            mobileTr.innerHTML = `
+                <th>${count}</th>
+                <td class="text-sm"">${shortTitle}</td>
+                <td class="font-semibold">$${product.price.toFixed(2)}</td>
+                <td><button class="btn btn-error btn-xs text-white" onClick="handleDeleteCartItem(${product.id})"><i class="fa-solid fa-trash"></i></button></td>
+            `;
+            cartTbody.append(tr);
+            mobileCartTbody.append(mobileTr);
+        }
+    });
+    cartTotalPriceEl.innerText = `$${currentTotal.toFixed(2)}`;
+    mobileCartTotalPriceEl.innerText = `$${currentTotal.toFixed(2)}`;
+}
+
+
+loadCartProductsList();
